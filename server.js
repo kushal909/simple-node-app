8322e85e66c53a1f928b1a6ad2a427ccd4e88830
kushal909 <kushalukumar909@gmail.com>
2025-07-27 16:07:20 +0530

    updated

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js App deployed on AWS...!");
+  res.send("Hello from Node.js App deployed on AWS... kushal!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on kushal http://localhost:${PORT}`);
 });
