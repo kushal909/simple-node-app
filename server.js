35e900ea37a86a01f610b4a5cb3cb811d9b1dd71
kushal909 <kushalukumar909@gmail.com>
2025-07-27 22:30:02 +0530

    updated

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Node.js App deployed on AWS... kushal!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on kushal http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on kushal http://3.110.27.124:${PORT}`);
 });
